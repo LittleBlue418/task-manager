b97b6b622cb05fa2349bd7799d0be9f5cfb51d71
LittleBlue418 <hollyathomas88@gmail.com>
2020-05-13 11:59:20 +0200

    app configuration + environment variables

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 import os
+
+from bson.objectid import ObjectId
+from dotenv import load_dotenv
 from flask import Flask
+from flask_pymongo import PyMongo
+
+load_dotenv()
 
 app = Flask(__name__)
+
+password = os.environ.get("MONGODB_PASSWORD")
+
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = 'mongodb+srv://root:{}@myfirstcluster-8zy3m.mongodb.net/task_manager?retryWrites=true&w=majority'.format(password)
+
+mongo = PyMongo(app)
 
 @app.route('/')
 def hello():
