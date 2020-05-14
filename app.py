57c9a5568f2b1d9dd803c98e9615bfa8c3635f9e
LittleBlue418 <hollyathomas88@gmail.com>
2020-05-14 23:11:47 +0200

    Edit & update categories

--- app.py
+++ app.py
@@ -102,6 +102,22 @@ def get_categories():
     return render_template('categories.html', categories=mongo.db.categories.find())
 
 
+@app.route('/edit_category/<category_id>')
+def edit_category(category_id):
+    return render_template('editcategory.html',
+        category=mongo.db.categories.find_one({'_id': ObjectId(category_id)}))
+
+
+@app.route('/update_category/<category_id>', methods=["POST"])
+def update_category(category_id):
+    categories = mongo.db.categories
+    categories.update({'_id': ObjectId(category_id)},
+        {
+            'category_name': request.form.get('category_name'),
+        })
+    return redirect(url_for('get_categories'))
+
+
 if __name__ == '__main__':
     app.run(
         host="0.0.0.0" if "DYNO" in os.environ else None,
