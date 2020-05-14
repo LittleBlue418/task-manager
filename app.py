a0069f72c0c5905e02151d3d1194c3417af9d7d2
LittleBlue418 <hollyathomas88@gmail.com>
2020-05-14 22:39:23 +0200

    delete task functionality

--- app.py
+++ app.py
@@ -90,6 +90,12 @@ def toggle_complete(task_id):
 
     return redirect(url_for('get_tasks'))
 
+
+@app.route('/delete_task/<task_id>')
+def delete_task(task_id):
+    mongo.db.tasks.remove({'_id': ObjectId(task_id)})
+    return redirect(url_for('get_tasks'))
+
 if __name__ == '__main__':
     app.run(
         host="0.0.0.0" if "DYNO" in os.environ else None,
